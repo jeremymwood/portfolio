836187c10173c155d7f543dda0884d916ab1d18b
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-05-17 15:33:19 -0500

    1533

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 "use strict";
 
-$('.codeup-icon').hover(function() {
-    $(this).addClass("d-none");
-    $('.codeup-icon-hover').removeClass("d-none");
-});
-
-$('.codeup-icon-hover').mouseleave(function() {
-    $(this).addClass("d-none");
-    $('.codeup-icon').removeClass("d-none");
-});
+// $('.codeup-icon').hover(function() {
+//     $(this).addClass("d-none");
+//     $('.codeup-icon-hover').removeClass("d-none");
+// });
+//
+// $('.codeup-icon-hover').mouseleave(function() {
+//     $(this).addClass("d-none");
+//     $('.codeup-icon').removeClass("d-none");
+// });
